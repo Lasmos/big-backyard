87dad3d26aeabc3b0d44af5036f6163db32bdee7
Lasmos <71170597+Lasmos@users.noreply.github.com>
2020-09-15 00:07:35 -0700

    Update bigBackyard.py
    Version 0.2 is still pretty basic.
    You can look at your inventory to see how many of each type of bugs you have. The game shows a corresponding message when you catch or fail to catch a bug, and every time you catch one you have to look for another one. The game only tells you on the main screen if you have a lot of bugs, not the specific number. Also I added plurals to the Bug object.

--- bigBackyard.py
+++ bigBackyard.py
@@ -4,7 +4,7 @@ import random
 
 #initialize gamewide variables
 #read the instructions!
-game_instructions = "Type 'c' to try catching the bug that's here.\n Type 'l' to look for a different bug.\nType 'e' to quit the game."
+game_instructions = "Type 'c' to try catching the bug that's here.\n Type 'l' to look for a different bug.\nType 'i' to check your inventory.\nType 'e' to quit the game."
 
 #bugs ever caught
 caught_bugs_ever = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]#11
@@ -40,23 +40,24 @@ class Bug ():
     "All the buzzing beasties, from the tiny to the ... less tiny."
     
     #every bug has these
-    def __init__(self, number, word, stringwords, intarray, enumsize):
+    def __init__(self, number, word, word2, stringwords, intarray, enumsize):
         global every_bug_type
         global every_bug_list
         
         self.id = number
         self.name = word
+        self.nameplural = word2
         self.description = stringwords
-        self.stats = intarray #height, swiftness, value
+        self.stats = intarray #height, swiftness, irregularity, value
         self.size = enumsize
         every_bug_type += 1
         every_bug_list.append(self)
         
 
 #create bugs
-morpho = Bug(1, "Blue Morpho", "A high-flying bright blue butterfly flits here.", [5,4,5], Sizes.SMALL)
-painted = Bug(2, "Painted Lady", "An orange butterfly with large black splotches has settled on a branch.", [2,2,2], Sizes.TINY)
-monarch = Bug(3, "Monarch", "A bright orange-and black butterfly dashes about.", [2,1,1], Sizes.TINY)
+morpho = Bug(1, "Blue Morpho", "Blue Morphos", "A high-flying bright blue butterfly flits here.", [5,4,5,5], Sizes.SMALL)
+painted = Bug(2, "Painted Lady", "Painted Ladies", "An orange butterfly with large black splotches has settled on a branch.", [2,2,2,2], Sizes.TINY)
+monarch = Bug(3, "Monarch", "Monarchs", "A bright orange-and black butterfly dashes about.", [2,1,1,1], Sizes.TINY)
 
 #bug-related methods
 def tradeidforbug(bugid):
@@ -80,28 +81,28 @@ def trytocatch(theBug):
     if(player_stats[1] > theBug.stats[0] * theBug.stats[1]):
         #they can probably catch the bug
         if(playerluck(75)):
-            iscaught(theBug)
+            return iscaught(theBug)
         else:
-            notcaught()
+            return notcaught()
     #if the player's jump is that good
     elif(player_stats[0] > theBug.stats[0]):
         #they can catch the bug if they're somewhat lucky
         if(playerluck(50)):
-            iscaught(theBug)
+            return iscaught(theBug)
         else:
-            notcaught()
+            return notcaught()
     elif(player_stats[1] > theBug.stats[1]):
         #they can catch the bug if they're pretty lucky
         if(playerluck(25)):
-            iscaught(theBug)
+            return iscaught(theBug)
         else:
-            notcaught()
+            return notcaught()
     else:
         #they might still be able to catch the bug
         if(playerluck(1 + (10 - theBug.stats[1]))):
-            iscaught(theBug)
+            return iscaught(theBug)
         else:
-            notcaught()
+            return notcaught()
 
     
 
@@ -110,8 +111,11 @@ def iscaught(theBug):
     global held_bugs_now
     global caught_bugs_ever
     
+    telluser("You got it!")
+    
     held_bugs_now[theBug.id] += 1
     caught_bugs_ever[theBug.id] += 1
+    return True
 
 def notcaught():
     lossmessage = "Better luck next time!"
@@ -125,6 +129,7 @@ def notcaught():
     elif(pickone == 4):
         lossmessage = "Time to try again."
     telluser(lossmessage)
+    return False
 
 
 def playerluck(tobeat):
@@ -142,30 +147,61 @@ def telluser(astring):
     print(astring)
 
 
-def addinventory():
+def addinventory(currentbugs):
     addingup = 0
-    for eachbugtype in held_bugs_now:
+    for eachbugtype in currentbugs:
         addingup += eachbugtype
     return addingup
 
+
+def inventory(current, potential):#array of ints, array of bugs
+    #what needs to be shown?
+    #for now just tell the player what bugs they have
+    checkingfornone = 0
+    
+    #for each bug type the player COULD have,
+    for bugtype in potential:
+        #if they DO have any
+        if(current[bugtype.id] > 0):
+            #tell them so
+            telluser(bugtype.nameplural + ": " + str(current[bugtype.id]))
+            #and add to checkingfornone so it won't be 0
+            checkingfornone += 1
+        #otherwise they don't have any so don't tell them about it
+    
+    #then check checkingfornone to see if it's empty
+    if(checkingfornone == 0):
+        telluser("You don't have any bugs. Such a sad empty inventory. Go catch some!")
+    
+    telluser("You have " + str(addinventory(current)) + " total bugs.")
+
+
+
+    
 
 
 #main gameplay
 
 #tell user how to play
 telluser(game_instructions)
+#randomize the first bug to show
+current_bug = giverandombug()
 
 #main game loop
 user_uses = True
 while(user_uses): 
-    #randomize and store the bug currently 'on-screen'
-    current_bug = giverandombug()
     
     #tell user what they have right now
-    telluser("You currently have " + str(addinventory()) + " bugs with you.")
+    if(addinventory(held_bugs_now) > 20):
+        telluser("You have quite a few bugs right now!")
+    elif(addinventory(held_bugs_now) > 50):
+        telluser("Wow, you have SO MANY bugs!")
     
     #tell user what's here right now
-    telluser("\n" + current_bug.description)
+    if(current_bug == None):
+        telluser("\nThere is no bug here right now. Look for one!")
+    else:
+        telluser("\n" + current_bug.description)
     
     #ask for user action
     state = input("What will you do? ")
@@ -173,20 +209,36 @@ while(user_uses):
     #respond to user action
     #if user wants to try to catch the bug,
     if(state == 'c' or state == 'C'):
-        #try to catch the bug
-        trytocatch(current_bug)
+        #if there is a bug present,
+        if(current_bug):
+            #try to catch the bug; 
+            if(trytocatch(current_bug)):
+                #if you succeeded, no bug is currently present
+                current_bug = None
+                #otherwise the bug that was already here is still here
+        #otherwise tell the user to look for another bug
+        else:
+            telluser("No bugs here; press 'l' to look for another.")
+    
     #if the user wants to look for a new bug,
     elif(state == 'l' or state == 'L'):
         #look for a new bug
         current_bug = giverandombug()
+    
     #if the user wants to leave the game,
     elif(state == 'e' or state == 'E'):
         #shut down while loop
         user_uses = False
+    
+    #if the user wants to check their inventory,
+    elif(state == 'i' or state == 'I'):
+        #list the bugs they currently have
+        inventory(held_bugs_now, every_bug_list)
+    
     #if the user inputs anything else,
     else:
         #tell them to pay attention this time
-        telluser("What is it you're trying to do? ")
+        telluser("I don't understand what you want.\n")
         telluser(game_instructions)
 
 
@@ -203,17 +255,31 @@ plans and stuff:
 -make bugs dodging you more interesting - /why/ did it escape?
 -give a you-caught-the-bug message
 -what does your inventory look like?
+-test the bug-catching system
+-player stats: jumping, aiming, and bargaining
+    jumping makes you able to catch bugs that fly too high
+    aiming makes the chance of missing lower
+    bargaining gives you a better chance of good deals from shopkeepers
+-bug stats: height, swiftness, irregularity, value
+    height makes them able to avoid you by going up
+    swiftness makes them generally harder to catch
+    irregularity makes them more difficult to predict
+    value makes them worth more
 
+-a boolean array for key items, you have it or you don't; the encyclopedia, types and levels of equipment?
 -make a bug encyclopedia - what bugs have you caught before? how do you identify what bugs you have?
--make bug size matter
+-make bug size matter - different nets are required to catch the smallest bugs and the larger bugs, and the largest ones need a different item entirely
+-make bug catching directional?
 -make shopkeepers to sell bugs to
 -make different spaces with different bugs
 -make a 'home base'?
 -make a training space, or just train stats with use? both?
--what types of items would make things easier to do?
+-what types of items would make things easier to do? net types for bug sizes
 -make a save system and ability to have different games going at one time
 -make lots more bugs
+-make bugs run away at some point? after a certain number of attempts?
 -WHERE IS GRANDPA
--what is the central struggle for this game? the main thing is to collect them all and get lots of money... but why?
+-what is the central struggle for this game? the main thing is to collect them all and get lots of money... but why? maybe your complete bug collection can get some money for Grandpa to help with his money problems. no, not the complete collection but one specific bug - diamond bug, or something. it's in a far away place that we have to figure out how to get to. 
+-maybe the shopkeepers can reference how odd it is that they're here paying so much money for bugs?
 
 """
